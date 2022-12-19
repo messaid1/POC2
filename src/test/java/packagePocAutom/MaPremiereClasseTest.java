72512435f57c5e96bce87af1eb1d39708067e81b
Mehdi ESSAID <messaid@norsys.fr>
2022-12-19 15:23:42 +0100

    Initial commit

---
+++ src/test/java/packagePocAutom/MaPremiereClasseTest.java
@@ -0,0 +1,73 @@
+package packagePocAutom;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.slf4j.LoggerFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class MaPremiereClasseTest {
+    private WebDriver driver;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MaPremiereClasseTest.class);
+    @BeforeEach
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver",   "/tmp/chromedriver");
+        //System.setProperty("webdriver.chrome.driver",   ".\\chromedriver.exe");
+        Map<String, Object> prefs = new HashMap<>();
+        // permet tous les cookies pour Chrome en deux etapes
+        // 1 - allow all cookies
+        prefs.put("profile.default_content_setting_values.cookies", 1);
+        // 2 - bloque seulement les cookies tiers : non
+        prefs.put("profile.block_third_party_cookies", false);
+        // Meme chose, pour Firefox
+        prefs.put("network.cookie.cookieBehavior", 0);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("window-size=1920x1080", "--headless", "--no-sandbox");
+        options.setExperimentalOption("prefs", prefs);
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Test POC ouverture google.fr")
+    public void POCGoogle(){
+        LOGGER.info("Ouverture de la page google.fr");
+        driver.get("http://www.google.fr");
+        driver.manage().window().maximize();
+        driver.findElement(By.cssSelector("#L2AGLb > div")).click();
+        LOGGER.info("Fin d'ouverture de google.fr");
+    }
+    @Test
+    @DisplayName("POCAssertionFausse")
+    public void POCAssertionFausse(){
+        try {
+            LOGGER.info("Essai d'assertion fausse");
+            Assertions.assertEquals(1,2);
+        }
+        catch (Exception e){
+            LOGGER.info("Assertion fausse, message : " + e);
+        }
+    }
+    @Test
+    @DisplayName("POCAssertionFausse")
+    public void POCAssertionVraie() {
+        try {
+            LOGGER.info("Essai d'assertion vraie");
+            Assertions.assertEquals(1, 1);
+            LOGGER.info("Assertion vraie, 1 = 1");
+        } catch (Exception e) {
+        }
+    }
+}
